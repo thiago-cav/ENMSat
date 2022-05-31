48aa67476106a06c63ab702c98f558fa79517b3b
Thiago Cavalcante <thiagocav.ferreira@gmail.com>
2022-05-31 15:54:07 -0300

    function 2

--- R/2-fun_sum.R
+++ R/2-fun_sum.R
@@ -1,4 +1,18 @@
-#function to sum vectors
+#' sum_vectors - function to sum vectors
+#' This function calculate richness of a vector of habitat suitability
+#'
+#'
+#' @param x a numerical vector of habitat suitability values
+#' @param ... arguments to be passed to internal functions
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples
+#' suit_vector <- c(0.3, 0.7, 0.1, 0, 0.1)
+#' sum_vectors(suit_vector)
+
+
 sum_vectors <- function(x, ...){
   r <- sum(x>0.5, ...)
   return(r)
