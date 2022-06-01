e44f3c7c076db32a72a80b4e72e239e0da50224f
Thiago Cavalcante <thiagocav.ferreira@gmail.com>
2022-06-01 10:23:26 -0300

    Last updates

--- R/2-fun_rich_from_suit.R
+++ R/2-fun_rich_from_suit.R
@@ -2,7 +2,7 @@
 #' This function calculate species richness based on an arbitrary threshold of
 #' 0.5 of suitability rasters
 #'
-#' @inheritParams sum_vectors
+#'
 #'
 #' @param x raster of Habitat suitability
 #' @param na.rm argument
@@ -11,7 +11,7 @@
 #' @export
 #'
 #' @examples
-#' spp_rich_suit()
+#'
 
 spp_rich_suit <- function(x, na.rm = FALSE){
   if(inherits(x, "SpatRaster")){
