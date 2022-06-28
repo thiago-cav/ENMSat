2f4bc5cd6af23bf4680fe83ff887321e1056b6c8
Thiago Cavalcante <thiagocav.ferreira@gmail.com>
2022-06-28 14:41:52 -0300

    Adding a raster file as DATA within the package. And adding the vignette for the extract_scales function.

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Remote sensing data file:
+#'
+#' The raster file is is median percentage of annual tree cover value per pixel
+#' estimated from Terra MODIS Vegetation Continuous Fields (VCF)
+#' The MODIS filter Date was 2003-01-01-2020-12-31
+#' @format raster file
+#'
+#' @source {created in Google Earth Engine to serve as example.}
+#' @example
+#' data(tree_cover)
+"tree_cover"
+
